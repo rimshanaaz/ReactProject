3e14643038fc0512869b8c305c637ea41fb5c868
unknown <rimshanaaz28@gmail.com>
2023-05-05 12:11:23 -0700

    My First Commit

---
+++ my-app/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,24 @@
+//component in react is just a javascript function!!!
+// lowercase characters - builtin  html elements eg: <h1> <p>
+// uppercase elements - custom elemnents mader by user <ExpenseItem>
+//attributes are knownm as props (properties) in react
+
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import React,{useState} from 'react';
+
+function ExpenseItem(props) {
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
